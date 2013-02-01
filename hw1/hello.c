3f4c849f9c5e9acb7322ad7b98377ad979e3c73b
mohamed camara <mcamara@cs.umb.edu>
2013-01-31 19:19:55 -0500

    updated hw1

--- hw1/hello.c
+++ hw1/hello.c
@@ -1,11 +1,11 @@
 /*Hello: first homework assignment
- name: Mohamed Camara
- date: 02/05/2013
+name: Mohamed Camara
+date: 02/05/2013
 */
 
 	#include <stdio.h>
 
 	main()
 	{
-		printf("Hello, World!\n");
+		printf("Hello World!\n");
 	}
